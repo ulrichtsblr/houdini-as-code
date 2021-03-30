17fbc82a052dab2efd6eae17a6daa21c14116190
ulrichtsblr <ulrichts.blr@protonmail.com>
2021-03-30 20:18:01 +0200

    update launchpad

--- launchpad.sh
+++ launchpad.sh
@@ -1,11 +1,26 @@
-script=donut.py
-script2=blender.py
+# user parameters
+script="main.py"
+houdini_version="18.5.499"
+operating_system="linux"
+
+# initialize navigation
 wdir=`pwd`
-hdir="C:\Program Files\Side Effects Software\Houdini 18.5.449"
+if [[ "$operating_system" -eq "linux" ]]
+then
+    hdir="/opt/hfs$houdini_version"
+else
+    if [[ "$operating_system" -eq "windows" ]]
+    then
+        hdir="C:\Program Files\Side Effects Software\Houdini $houdini_version"
+    else
+        echo "invalid operating system"
+    fi
+fi
 
-
+# initialize houdini environment
 cd "$hdir"
 source houdini_setup > /dev/null 2>&1
 
+# run hython script
 cd "$wdir"
 hython $script
