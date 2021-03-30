ed5a40f9f22173865479ed6ac51d9e7322e57f09
ulrichtsblr <ulrichts.blr@protonmail.com>
2021-03-30 23:26:09 +0200

    refactoring

---
+++ hac.py
@@ -0,0 +1,133 @@
+import hou
+import os
+
+class Node:
+
+
+    def __init__(self, node="camera"):
+        if node == "camera":
+            self.node = (
+                hou.node("/obj")
+                .createNode("cam", "CameraInstance")
+            )
+        if node == "box":
+            self.node = (
+                hou.node("/obj")
+                .createNode("geo", "AbstractBoxGeometry")
+                .createNode("box", "BoxInstance")
+            )
+        if node == "sphere":
+            self.node = (
+                hou.node("/obj")
+                .createNode("geo", "AbstractSphereGeometry")
+                .createNode("sphere", "SphereInstance")
+            )
+        if node == "torus":
+            self.node = (
+                hou.node("/obj")
+                .createNode("geo", "AbstractTorusGeometry")
+                .createNode("torus", "TorusInstance")
+            )
+        return None
+
+
+    def set_xyz(self, v):
+        self.node.parmTuple("t")[0].set(str(v[0]))
+        self.node.parmTuple("t")[1].set(str(v[1]))
+        self.node.parmTuple("t")[2].set(str(v[2]))
+        return None
+
+
+    def get_xyz(self):
+        xyz = self.node.parmTuple("t").eval()
+        return xyz
+
+
+class Generator:
+
+
+    def __init__(self):
+        return None
+
+
+    def add_camera(self, v=(0, 0, 0)):
+        node = Node("camera")
+        node.set_xyz(v)
+        return node
+
+
+    def add_box(self, v=(0, 0, 0)):
+        node = Node("box")
+        node.set_xyz(v)
+        return node
+
+
+    def add_sphere(self, v=(0, 0, 0)):
+        node = Node("sphere")
+        node.set_xyz(v)
+        return node
+
+
+    def add_torus(self, v=(0, 0, 0)):
+        node = Node("torus")
+        node.set_xyz(v)
+        return node
+
+
+class DirectedAcyclicGraph:
+
+
+    def __init__(self, scene=None):
+        if not scene:
+            scene = os.path.join("scenes", "seed.hipnc")
+        hou.hipFile.load(scene)
+        return None
+
+
+    def export_png(self, schema):
+        opengl = (
+            hou.node("/obj")
+            .createNode("ropnet","RopNetInstance")
+            .createNode("opengl","OpenGLInstance")
+        )
+        opengl.parm("camera").set("/obj/CameraInstance")
+        opengl.parm("picture").set("$HIP/{}.png".format(schema))
+        opengl.parm("execute").pressButton()
+        return None
+
+
+    def export_hip(self, schema):
+        hou.hipFile.save(os.path.join("scenes", "{}.hipnc".format(schema)))
+        return None
+
+
+    def render(self, schema="schema"):
+        self.export_png(schema)
+        self.export_hip(schema)
+        return None
+
+
+###############################################################################
+
+
+# def generate_object_network():
+#     object_network = hou.node("/obj")
+#     torus_geom = object_network.createNode("geo", "AbstractTorusGeometry")
+#     scatter_instance = torus_geom.createNode("scatter", "scatter1")
+#     scatter_instance.setInput(0, torus_instance, 0)
+#     return None
+
+
+# def export_obj(self):
+#     torus_geom = hou.node("/obj/AbstractTorusGeometry")
+#     torus = hou.node("/obj/AbstractTorusGeometry/donut")
+#     file = torus_geom.createNode("file","export")
+#     #connect inputs first
+#     file.setInput(0,torus,0)
+#     file.parm("filemode").set("write")
+#     file.parm("file").set("$HIP/donut.obj")
+#     null = torus_geom.createNode("null","Null")
+#     null.setInput(0,file,0)
+#     null.setDisplayFlag(1)
+#     file.parm("reload").pressButton()
+#     return None
--- main.py
+++ main.py
@@ -1,34 +1,31 @@
-# import hou not required
+from hac import *
 
 
-def generate_object_network():
-    # object network and primary geometries
-    object_network = hou.node("/obj")
-    sphere_geom = object_network.createNode("geo", "AbstractSphereGeometry")
-    box_geom = object_network.createNode("geo", "AbstractBoxGeometry")
-    torus_geom = object_network.createNode("geo", "AbstractTorusGeometry")
-    
-    # nodes
-    sphere1 = sphere_geom.createNode("sphere", "sphere1")
-    sphere2 = sphere_geom.createNode("sphere", "sphere2")
-    sphere3 = sphere_geom.createNode("sphere", "sphere3")
-    box1 = box_geom.createNode("box", "box1")
-    box2 = box_geom.createNode("box", "box2")
-    box3 = box_geom.createNode("box", "box3")
-    torus1 = torus_geom.createNode("torus", "torus1")
-    torus2 = torus_geom.createNode("torus", "torus2")
-    torus3 = torus_geom.createNode("torus", "torus3")
-    scatter1 = torus_geom.createNode("scatter", "scatter1")
-    scatter1.setInput(0, torus1, 0)
+def generate_donut():
+    gen = Generator()
+    nod0 = gen.add_box(v=(1, 0, 0))
+    nod1 = gen.add_sphere(v=(0, 1, 0))
+    nod2 = gen.add_torus(v=(0, 0, 1))
+    print(nod0.get_xyz())
+    print(nod1.get_xyz())
+    print(nod2.get_xyz())
+    return None
+
+
+def generate_camera():
+    gen = Generator()
+    cam = gen.add_camera(v=(0, 0, 10))
+    print(cam.get_xyz())
     return None
 
 
 def main():
-    infile = "base_scene.hipnc"
-    outfile = "base_scene_with-net.hipnc"
-    hou.hipFile.load(infile)
-    generate_object_network()
-    hou.hipFile.save(outfile)
+    dag = DirectedAcyclicGraph()
+    generate_donut()
+    generate_camera()
+    dag.render(schema="donut")
     return None
 
-main()
+
+if __name__ == "__main__":
+    main()
